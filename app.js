51e3e61e2dbce751c1089ab21b240669a0d7ade1
Grayson Croom <grayson.croom@gmail.com>
2017-04-26 19:59:05 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const bodyParser = require('body-parser'),
+         express = require('express'),
+             app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+const pageArray = ['types', 'bibliography', 'materials', 'about'];
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+app.get('/:page', (req, res) => {
+
+	var pageRequest  = req.params.page.toLowerCase(),
+	    validRequest = false;
+	
+	pageArray.forEach(e => { 
+		if(e === pageRequest) {
+			validRequest = true;
+		}
+	}
+	
+	res.render(validRequest ? pageRequest : 'error');
+});
+
+
+app.listen(8080, 'localhost', () => {
+	console.log('The server has started on ' + 'localhost' + ':' + 8080);
+});
